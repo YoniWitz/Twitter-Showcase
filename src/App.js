2c969b59c43dc474fa21a7abf61346e1cc20e28c
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-21 16:46:30 -0500

    added router with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import MyNavbar from './components/MyNavbar';
+import Home from './components/Home';
+import User from './components/User';
+import Random from './components/Random';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <MyNavbar/>
+      <Router>
+        <MyNavbar />
+        <Switch>
+          <Route path="/" component={Home} />           
+          <Route path="/user" component={User} />         
+          <Route path="/random" component={Random} />          
+        </Switch>
+      </Router>
     </div>
   );
 }
