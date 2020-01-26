a2d5f5b8c88579b3d0077ed30ca4d7c7a0c6d666
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-26 18:45:22 -0500

    fixed fe

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import MyCard from './MyCard'
 import axios from 'axios'
+import { Button, Form, Row, Col } from 'react-bootstrap'
 
 function User() {
     const [cards, setCards] = useState([])
@@ -15,11 +16,31 @@ function User() {
             )
     }, [])
 
+    async function handleSubmit(event) {
+        event.preventDefault();
+        axios.get(`http://localhost:3001/api/tweets/random`)
+            .then(response =>
+                setCards(response.data)
+            )
+            .catch(error =>
+                console.log(error.message)
+            )
+    }
+
     let renderCards = cards.map(data => <MyCard key={data.id} data={data} />);
 
     return (
         <main>
-            {renderCards}
+            <Form onSubmit={handleSubmit} style={{marginTop:"10px"}}>
+                <Row>
+                    <Col></Col>
+                    <Col>
+                        <Button variant="info" onClick={handleSubmit}>Generate Tweets from my Top 5 People</Button>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Form>           
+            {renderCards}           
         </main>
     )
 }
