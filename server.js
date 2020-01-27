416f0d355bd534572bf78851a1e689ca9108ff93
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-27 13:26:06 -0500

    package.json

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(express.static(__dirname + "/dist"));
+app.get(/.*/, function(req, res){
+    res.sendfile(__dirname+"/dist/index.html");
+});
 
 let TwitterController = require('./controllers/twitter-controller')
 
